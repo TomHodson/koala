219399679f196eefb5235b92d352a3dd5965be86
Tom Hodson <thomas.c.hodson@gmail.com>
2022-08-02 11:18:48 +0100

    fix pathfinding order

--- tests/test_flux_finder.py
+++ tests/test_flux_finder.py
@@ -9,15 +9,17 @@ from koala.flux_finder.pathfinding import path_between_plaquettes, path_between_
 # make some lattices to test
 n = 15
 honeycomb, _ = example_graphs.generate_honeycomb(n, return_coloring=True)
-random_points = pointsets.generate_random(n*n)
+random_points = pointsets.generate_random(n * n)
 amorphous = voronization.generate_lattice(random_points, shift_vertices=True)
+
 
 def find_random_flux_sector(l):
     """Tries to find a random flux sector."""
-    random_fluxes = np.random.choice([-1, 1], size = l.n_plaquettes)
+    random_fluxes = np.random.choice([-1, 1], size=l.n_plaquettes)
     bonds = find_flux_sector(l, random_fluxes)
     fluxes = fluxes_from_bonds(l, bonds)
     return bonds, fluxes
+
 
 def test_flux_finder():
     """find_flux_sector checks for itself if it finds an output within one flux of the input"""
@@ -28,10 +30,14 @@ def test_flux_finder():
 
 def test_pathfinding():
     for lattice in [honeycomb, amorphous]:
-        a,b = np.random.randint(lattice.n_vertices, size = (2,))
-        vertices, edges = path_between_vertices(lattice, a, b, early_stopping = False)
-        vertices, edges = path_between_vertices(lattice, a, b, early_stopping = True)
+        a, b = np.random.randint(lattice.n_vertices, size=(2,))
+        vertices, edges = path_between_vertices(lattice, a, b, early_stopping=False)
+        vertices, edges = path_between_vertices(lattice, a, b, early_stopping=True)
+        assert vertices[0] == a
+        assert vertices[-1] == b
 
-        a,b = np.random.randint(lattice.n_plaquettes, size = (2,))
-        plaquettes, edges = path_between_plaquettes(lattice, a, b, early_stopping = False)
-        plaquettes, edges = path_between_plaquettes(lattice, a, b, early_stopping = True)
+        a, b = np.random.randint(lattice.n_plaquettes, size=(2,))
+        plaquettes, edges = path_between_plaquettes(lattice, a, b, early_stopping=False)
+        plaquettes, edges = path_between_plaquettes(lattice, a, b, early_stopping=True)
+        assert plaquettes[0] == a
+        assert plaquettes[-1] == b
